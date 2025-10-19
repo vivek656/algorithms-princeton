f39591023c61727a1d5732db66732a3a3a4a823e
vivek singh latwal <vivekaltwal656@gail.com>
2025-10-19 15:49:06 +0530

    mm

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,6 +4,9 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Arrays;
 
 public class Board {
@@ -14,6 +17,7 @@ public class Board {
     private int hamming = -1;
     private int manhattan = -1;
     private int dimension;
+    private int blankPosition;
 
     public Board(int[][] tiles) {
         this.tiles = new int[tiles.length][tiles[0].length];
@@ -21,6 +25,7 @@ public class Board {
             this.tiles[i] = Arrays.copyOf(tiles[i], tiles[0].length);
         }
         this.dimension = tiles.length;
+        initializeAll();
     }
 
     // string representation of this board
@@ -43,41 +48,17 @@ public class Board {
 
     // number of tiles out of place
     public int hamming() {
-        if (hamming != -1) return hamming;
-        int outOfPlace = 0;
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                if (isBlank(i, j)) continue;
-                int myplace = i * dimension + j + 1;
-                if (tiles[i][j] != myplace) {
-                    outOfPlace++;
-                }
-            }
-        }
-        hamming = outOfPlace;
         return hamming;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        if (manhattan != -1) return manhattan;
-        int sum = 0;
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                if (isBlank(i, j)) continue;
-                int myplace = i * dimension + j + 1;
-                if (tiles[i][j] != myplace) {
-                    sum += dinstanceToOptimal(i, j);
-                }
-            }
-        }
-        manhattan = sum;
-        return sum;
+        return manhattan;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-
+        return true;
     }
 
     // does this board equal y?
@@ -96,13 +77,33 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
+        Queue<Board> q = new Queue<Board>();
+        int[] blankPos = posAsArray(blankPosition);
+        if (blankPos[0] != 0) {
+            q.enqueue(exchnageAndGetNew(blankPos, new int[] { blankPos[0] - 1, blankPos[1] }));
+        }
+        if (blankPos[1] != dimension - 1) {
+            q.enqueue(exchnageAndGetNew(blankPos, new int[] { blankPos[0], blankPos[1] + 1 }));
 
+        }
+        if (blankPos[0] != dimension - 1) {
+            q.enqueue(exchnageAndGetNew(blankPos, new int[] { blankPos[0] + 1, blankPos[1] }));
+
+        }
+        if (blankPos[1] != 0) {
+            q.enqueue(exchnageAndGetNew(blankPos, new int[] { blankPos[0], blankPos[1] - 1 }));
+        }
+        return q;
     }
+
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-
+        int random1 = randomNonBlankPosition(-1);
+        int random2 = randomNonBlankPosition(random1);
+        return exchnageAndGetNew(posAsArray(random1), posAsArray(random2));
     }
+
 
     private boolean isBlank(int i, int j) {
         return tiles[i][j] == 0;
@@ -121,5 +122,66 @@ public class Board {
         return (optimalVertical - (i + 1)) + (optimalHorizontal - (j + 1));
     }
 
+    private void initializeAll() {
+        int manhattanSum = 0;
+        int hammingSum = 0;
+        int[] blankPos = new int[2];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (isBlank(i, j)) {
+                    blankPos[0] = i;
+                    blankPos[1] = j;
+                    continue;
+                }
+                int myplace = i * dimension + j + 1;
+                if (tiles[i][j] != myplace) {
+                    manhattanSum += dinstanceToOptimal(i, j);
+                    hammingSum += 1;
+                }
+            }
+        }
+        manhattan = manhattanSum;
+        hamming = hammingSum;
+        blankPosition = blankPos[0] * dimension + blankPos[1];
+    }
+
+    private int randomNonBlankPosition(int except) {
+        while (true) {
+            int pos = StdRandom.uniformInt(0, dimension * dimension);
+            if (pos != blankPosition && pos != except) {
+                return pos;
+            }
+        }
+    }
+
+    private int[] posAsArray(int pos1) {
+        return new int[] { (pos1) / dimension, dimension - pos1 % dimension };
+    }
+
+    private int[] neighboursPos() {
+        int[] blankPos = posAsArray(blankPosition);
+        int[] neighbours = new int[4];
+        if (blankPos[0] != 0) {
+            neighbours[0] = 1;
+        }
+        if (blankPos[1] != dimension - 1) {
+            neighbours[1] = 1;
+        }
+        if (blankPos[0] != dimension - 1) {
+            neighbours[2] = 1;
+        }
+        if (blankPos[1] != 0) {
+            neighbours[3] = 1;
+        }
+        return neighbours;
+    }
+
+    private Board exchnageAndGetNew(int[] posi1, int[] posi2) {
+        Board b = new Board(tiles);
+        int temp = b.tiles[posi1[0]][posi1[1]];
+        b.tiles[posi1[0]][posi1[1]] = b.tiles[posi2[0]][posi2[1]];
+        b.tiles[posi2[0]][posi2[1]] = temp;
+        return b;
+    }
 
 }
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -4,9 +4,8 @@
  *  Description:
  **************************************************************************** */
 
-package PACKAGE_NAME;
 public class Solver {
     public static void main(String[] args) {
-        
+
     }
 }
